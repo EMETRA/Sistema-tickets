3d8ee1aa6e41822a00ac3e0ec06d0888f2a21e43
Diego Hernandez <dahernandezsilvestre@gmail.com>
2026-03-13 11:00:24 -0600

    feat: added profile page

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,4 +1,4 @@
-import ForgotPassword from "../../../pages/ForgotPassword/ForgotPassword";
+import ForgotPassword from "@/pages/ForgotPassword/ForgotPassword";
 
 export default function ForgotPasswordPage() {
     return <ForgotPassword />;
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import Login from "../../../pages/Login/Login";
+import Login from "@/pages/Login/Login";
 
 export default function LoginPage() {
     return <Login />;

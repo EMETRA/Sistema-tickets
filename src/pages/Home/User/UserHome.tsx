a492806fbd3d1aef92cbe9c3339ad5ecbb602c3e
Diego Hernandez <dahernandezsilvestre@gmail.com>
2026-05-27 12:48:04 -0600

    feat: changed labels

--- src/pages/Home/User/UserHome.tsx
+++ src/pages/Home/User/UserHome.tsx
@@ -98,9 +98,9 @@ const UserHome: React.FC = () => {
         return <div className={styles.mainContainer}>Cargando información...</div>;
     }
 
-    if (error) {
-        return <div className={styles.mainContainer}>{error}</div>;
-    }
+    // if (error) {
+    //     return <div className={styles.mainContainer}>{error}</div>;
+    // }
 
 
     return (

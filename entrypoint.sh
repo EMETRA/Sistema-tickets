94e3654bbbf5206f3e6a0f08927182bc2ff876e9
hsilvaM <hsilva@muniguate.com>
2026-06-18 16:46:05 -0600

    [fix]: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ export PATH=$(pnpm bin):$PATH
 export NODE_ENV=production
 
 APP_PORT=${APP_PORT:-3000}
+export PORT="${APP_PORT}"
 
-echo "Iniciando servidor..."
-exec pnpm start -- -p "${APP_PORT}"
+echo "Iniciando servidor en puerto ${PORT}..."
+exec pnpm start
